c6e5dcd73432b559fc1dde91bc60fb8cb896f477
Morveusz93 <morveusz93@gmail.com>
2021-10-28 19:06:55 +0200

    add function watermark_with_text()

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 from os import listdir, mkdir
 from os.path import join
 
@@ -8,19 +8,13 @@ from os.path import join
 def marking(dir_name, images, watermark, saving_dir):
     for img in images:
         image = Image.open(join(dir_name, img))
-        # get size of image and watermark to calculate the position of watermark
         wm_width, wm_height = watermark.size
         image_width, image_height = image.size
         wm_pos = (image_width - wm_width, image_height - wm_height)
-        # paste watermark into image
-        # try: if there is opacity, we need to add mask - the third argument
         try:
             image.paste(watermark, wm_pos, watermark)
-        # if there is'nt opacity paste will raise 'ValueError', then we simply paste watermark without mask
         except ValueError:
             image.paste(watermark, wm_pos)
-        # save the image with watermark. 
-        # if there is no directory created ('marked') it will raise except 'FileNotFoundError' then we will create the dir
         try:
             image.save(join(saving_dir, img))
         except FileNotFoundError:
@@ -29,17 +23,34 @@ def marking(dir_name, images, watermark, saving_dir):
 
 
 
+def marking_with_text(dir_name, images, saving_dir):
+    for img in images:
+        image = Image.open(join('images', img)).convert('RGBA')
+        text_img = Image.new('RGBA', image.size, (255,255,255,0))
+        font = ImageFont.truetype("arial.ttf", 40)
+        draw = ImageDraw.Draw(text_img)
+        draw.text(xy=(0, 0), text="SIEMA", font=font, fill=(255,255,255,255))
+        marked_img = Image.alpha_composite(image, text_img).convert('RGB')
+
+        try:
+            marked_img.save(join(saving_dir, img))
+        except FileNotFoundError:
+            mkdir(saving_dir)
+            marked_img.save(join(saving_dir, img))
+
+
+
+
+
 def main():
     #---------------------------- images ----------------------------#
     print("Name of dir with images: ")
     dir_name = input().lower()
     saving_dir = join(dir_name, 'marked')
-    # create list of files in directory with images
     images = [file for file in listdir(dir_name) if file.endswith(('.jpg', '.jpeg', '.png'))]
 
     #---------------------------- watermark ----------------------------#
     print("\nfilename of your watermark:")
-    # path to watermark
     wm = input().lower()
     watermark = Image.open(wm)
 
@@ -48,7 +59,6 @@ def main():
     opacity = int(input())
     watermark.putalpha(opacity)
 
-    # size of watermark
     print("size of your watermark: ")
     wm_width, wm_height = input().split()
     wm_width, wm_height = int(wm_width), int(wm_height)
